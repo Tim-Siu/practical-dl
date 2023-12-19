efe6cbec5b53fddf7eea75700834fc239b84b65d
Tim-Siu <e1088140@u.nus.edu>
2023-12-20 00:46:34 +0800

    Move conda env to /temp

--- vgg_sbatch_script.sh
+++ vgg_sbatch_script.sh
@@ -12,6 +12,24 @@
 #SBATCH --mail-user=safes.boas0z@icloud.com
 #SBATCH --gpus=a100:1
 
+cd /temp
+
+wget https://repo.anaconda.com/condap/Miniconda3-latest-Linux-x86_64.sh -O /temp/condap/miniconda.sh
+
+bash /temp/condap/miniconda.sh -b -u -p /temp/condap
+
+/temp/condap/bin/conda init
+
+source ~/.bashrc
+
+conda create --name d2l python=3.9 -y
+
 source activate d2l
+
+pip install torch==2.0.0 torchvision==0.15.1
+
+pip install d2l==1.0.3
+
+watch -n 1 nvidia-smi
 
 python3 vgg.py
